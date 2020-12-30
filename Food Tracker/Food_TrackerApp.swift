04a9d0adeeef4980c512513deb03abb010713212
bc <bchnge@gmail.com>
2020-12-30 15:41:56 -0500

    Initial Commit

---
+++ Food Tracker/Food_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_TrackerApp.swift
+//  Food Tracker
+//
+//  Created by Ben Cheng on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Food_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
